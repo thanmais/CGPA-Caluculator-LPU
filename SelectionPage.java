4c8981f43fcc8938e4857d373886b4c2c1d6c329
thanmais <111916408+thanmais@users.noreply.github.com>
2023-04-29 18:36:46 +0530

    src

---
+++ SelectionPage.java
@@ -0,0 +1,108 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SelectionPage implements ActionListener {
+
+    JButton button = new JButton();
+    JButton button2 = new JButton();
+
+    SelectionPage() {
+
+        JLabel label = new JLabel();
+
+        // label.setText("Hi");
+
+        // JButton button = new JButton();
+        button.setBounds(100, 100, 190, 190);
+        label.add(button);
+
+        // ImageIcon icon = new ImageIcon("GPA1.png");
+        // button.setIcon(icon);
+        button.setText("TGPA Calculation");
+        button.setForeground(Color.BLACK);
+        button.setBackground(Color.pink);
+        button.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        /*
+         * button.addActionListener(new ActionListener() {
+         * public void actionPerformed(ActionEvent e) {
+         * 
+         * 
+         * selection1page m1 = selection1page();
+         * }
+         * });
+         */
+
+        // JButton button2 = new JButton();
+        button2.setBounds(400, 100, 190, 190);
+        button2.setText("CGPA Calculation");
+        button2.setForeground(Color.BLACK);
+        button2.setBackground(Color.LIGHT_GRAY);
+        button2.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+        /*
+         * button2.addActionListener(new ActionListener() {
+         * public void actionPerformed(ActionEvent e) {
+         * 
+         * JFrame frame2 = new JFrame();
+         * frame2.setSize(100,100);
+         * frame2.setVisible(true);
+         * frame.setVisible(false);
+         * 
+         * 
+         * 
+         * }
+         * });
+         */
+        label.add(button2);
+
+        // ImageIcon icon2 = new ImageIcon("GPA1.png");
+        // button.setIcon(icon2);
+
+        JFrame frame = new JFrame();
+        frame.setSize(700, 450);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(new Color(0, 0, 0));
+        frame.add(label);
+
+        // frame.add(panel);
+
+        // frame.add(button);
+
+        frame.setVisible(true);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                // JFrame frame2 = new JFrame();
+                // frame2.setSize(100,100);
+                // frame2.setVisible(true);
+                frame.setVisible(false);
+                // window2 w2 = new window2();
+                // w2.showWindow();
+
+                TGPACalculator w2 = new TGPACalculator();
+
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                frame.setVisible(false);
+                CGPACalculator W1 = new CGPACalculator();
+            }
+        });
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
